3b82e7e39f7720d0b536b22524c5704ef919dd87
Melvin Wahlberg <mellepella@gmail.com>
2021-08-27 09:43:12 +0200

    Add UIBinds class

---
+++ scripts/UIBinds.js
@@ -0,0 +1,40 @@
+class UIBinds {
+  static frameHandler = {
+    newFrame: () => {
+      FrameHandler.newFrame(Application.frameTemplate);
+    },
+    play: () => {
+      FrameHandler.play();
+    },
+    showNextFrame: () => {
+      FrameHandler.showNextFrame();
+    },
+    showPreviousFrame: () => {
+      FrameHandler.showPreviousFrame();
+    },
+  };
+  static pen = {
+    enableDraw: () => {
+      Application.pen.changeMethod("draw");
+    },
+    enableErase: () => {
+      Application.pen.changeMethod("erase");
+    },
+  };
+  static overlay = {
+    src: document.getElementById("canvas-overlay"),
+    getCtx: () => {
+      return this.overlay.src.getContext("2d");
+    },
+    draw: ({ x, y }) => {
+      const size = this.overlay.size;
+      this.overlay.getCtx().fillRect(x, y, size, size);
+    },
+    clear: () => {
+      const canvasHeight = this.overlay.src.height;
+      const canvasWidth = this.overlay.src.width;
+      this.overlay.getCtx().clearRect(0, 0, canvasWidth, canvasWidth);
+    },
+    size: Application.UNIT_SIZE,
+  };
+}
--- scripts/userInterface.js
+++ scripts/userInterface.js
@@ -1,59 +1,19 @@
 class UserInterface {
   static tools = { slider: document.getElementById("frames-slider") };
   static info = { sliderLabel: document.getElementById("frames-slider-label") };
-  static binds = {
-    frameHandler: {
-      newFrame: () => {
-        FrameHandler.newFrame(Application.frameTemplate);
-      },
-      play: () => {
-        FrameHandler.play();
-      },
-      showNextFrame: () => {
-        FrameHandler.showNextFrame();
-      },
-      showPreviousFrame: () => {
-        FrameHandler.showPreviousFrame();
-      },
-    },
-    pen: {
-      enableDraw: () => {
-        Application.pen.changeMethod("draw");
-      },
-      enableErase: () => {
-        Application.pen.changeMethod("erase");
-      },
-    },
-    overlay: {
-      src: document.getElementById("canvas-overlay"),
-      getCtx: () => {
-        return this.binds.overlay.src.getContext("2d");
-      },
-      draw: ({ x, y }) => {
-        const size = this.binds.overlay.size;
-        this.binds.overlay.getCtx().fillRect(x, y, size, size);
-      },
-      clear: () => {
-        const canvasHeight = this.binds.overlay.src.height;
-        const canvasWidth = this.binds.overlay.src.width;
-        this.binds.overlay.getCtx().clearRect(0, 0, canvasWidth, canvasWidth);
-      },
-      size: Application.UNIT_SIZE,
-    },
-  };
 
   static adjustOverlaySize() {
-    this.binds.overlay.src.width = Application.frameTemplate.width;
-    this.binds.overlay.src.height = Application.frameTemplate.height;
+    UIBinds.overlay.src.width = Application.frameTemplate.width;
+    UIBinds.overlay.src.height = Application.frameTemplate.height;
   }
 
   static handleClick({ subject, action }) {
-    this.binds[subject][action]();
+    UIBinds[subject][action]();
   }
 
   static onOverlayMove(x, y) {
-    this.binds.overlay.clear();
-    this.binds.overlay.draw({ x, y });
+    UIBinds.overlay.clear();
+    UIBinds.overlay.draw({ x, y });
   }
 
   static sliderLabelRefresh(currentFrame) {
