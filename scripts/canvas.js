91bcce3263a10336db23f11c73aeb38ce54bb9d0
Melvin Wahlberg <mellepella@gmail.com>
2021-11-27 14:25:55 +0100

    Make canvas copy content when created
    This commits makes canvases copy the one before it when creating a new one.

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -21,6 +21,11 @@ class Canvas {
     this.ctx.fillRect(x, y, width, height);
   }
 
+  copy(target) {
+    const content = FrameHandler.frames[target].src;
+    this.ctx.drawImage(content, 0, 0);
+  }
+
   createId() {
     if (this.id) {
       return this.id;
@@ -28,7 +33,7 @@ class Canvas {
     return FrameHandler.frames.length;
   }
 
-  drawBg({ color }) {
+  drawBg(color) {
     this.ctx.fillStyle = color;
     this.ctx.fillRect(0, 0, this.width, this.height);
   }
@@ -49,6 +54,9 @@ class Canvas {
     this.create();
     this.src = DocumentHandler.find(this.id);
     this.ctx = this.src.getContext("2d");
+    if (this.id != 0) {
+      this.copy(this.id - 1);
+    }
     return true;
   }
 }
